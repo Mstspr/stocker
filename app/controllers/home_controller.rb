5db90525c2c474cd986cdd253f7c1c88bfcdda12
Mstspr <mstspr1155@gmail.com>
2017-12-18 22:31:05 +0000

    Add Quote

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
   def index
+    if params[:id] == ""
+      @nothing = "Hey, You forget to enter  a symbol"
+    elsif
+    
+      if params[:id]
+          begin
+              @stock = StockQuote::Stock.quote(params[:id])
+          rescue StandardError
+              @error = "This Symbol doesn't Exist.. please try agian"
+          end
+      end
+    end
   end
   
   def about
